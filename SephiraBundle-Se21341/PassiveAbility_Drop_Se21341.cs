66ffb8b48452b72d7de2375d5da741f90123591b
Kamiyo <kamiyo@outlook.it>
2021-12-17 22:43:48 +0100

    Adding Drop Passive

---
+++ SephiraBundle-Se21341/PassiveAbility_Drop_Se21341.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SephiraBundle_Se21341.Models;
+using UnityEngine;
+
+namespace SephiraBundle_Se21341
+{
+    public class PassiveAbility_Drop_Se21341 : PassiveAbilityBase
+    {
+    }
+}
