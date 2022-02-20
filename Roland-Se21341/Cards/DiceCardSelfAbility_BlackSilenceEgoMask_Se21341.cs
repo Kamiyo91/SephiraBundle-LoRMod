a9c3149ed191cd904cd31be425d98e223c19ad40
Kamiyo <kamiyo@outlook.it>
2022-02-20 16:25:28 +0100

    Ego Emotion Change Level 3

--- Roland-Se21341/Cards/DiceCardSelfAbility_BlackSilenceEgoMask_Se21341.cs
+++ Roland-Se21341/Cards/DiceCardSelfAbility_BlackSilenceEgoMask_Se21341.cs
@@ -4,7 +4,7 @@
     {
         public override bool OnChooseCard(BattleUnitModel owner)
         {
-            return (owner.emotionDetail.EmotionLevel >= 4 ||
+            return (owner.emotionDetail.EmotionLevel >= 3 ||
                     owner.passiveDetail.PassiveList.Exists(x =>
                         !x.destroyed && x.id == new LorId("LorModPackRe21341.Mod", 61))) &&
                    !owner.bufListDetail.HasAssimilation();
